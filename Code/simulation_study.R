856d9a77dc6e46599bfe068afd90d575ca349805
JonathanWillnow <82315811+JonathanWillnow@users.noreply.github.com>
2021-12-08 21:29:25 +0100

    New approach, implementing GCV / 10kcv

--- Code/simulation_study.R
+++ Code/simulation_study.R
@@ -1,5 +1,4 @@
 rm(list=ls())
-
 data_generation <-function(fun){
     var1 = 1
     var2 = 2
@@ -17,7 +16,6 @@ data_generation <-function(fun){
            X[i2,]=X[i2,]+e[1]*sin((2*pi)*grid*j2)
            X[i2,]=X[i2,]+e[2]*cos((2*pi)*grid*j2)
         }
-        
     }
     return(X)
 }
@@ -25,9 +23,9 @@ data_generation <-function(fun){
 data(gasoline)
 octane <- (gasoline$octane)
 NIR    <- as.matrix(gasoline$NIR)
-test_size = 20
-
+###################################################################################################################
 # set up "global" variables
+###################################################################################################################
 set.seed(100)
 n_obs = 60
 nharm = 4
@@ -37,125 +35,107 @@ grid = seq(0, 1, length = n_var+1)
 f1 <- 2*sin(0.5*pi*grid) + 4*sin(1.5*pi*grid) + 5*sin(2.5*pi*grid)
 #bumpy
 f2 <- 1.5*exp(-0.5*(grid-0.3)^2/0.02^2) -  4*exp(-0.5*(grid-0.45)^2/0.015^2) +  8*exp(-0.5*(grid-0.6)^2/0.02^2) -  exp(-0.5*(grid-0.8)^2/0.03^2)
-
 #two different variances of error
-#sigma_eps_squared1 = as.numeric(2/3*var(NIR %*% f1))
-#sigma_eps_squared2 = as.numeric(1/9*var(NIR %*% f1))
 sigma_eps_squared1_1 = as.numeric((var(NIR %*% f1)/0.9) - var(NIR %*% f1))
 sigma_eps_squared1_2 = as.numeric((var(NIR %*% f1)/0.6) - var(NIR %*% f1) )
 sigma_eps_squared2_1 = as.numeric((var(NIR %*% f2)/0.9) - var(NIR %*% f2))
 sigma_eps_squared2_2 = as.numeric((var(NIR %*% f2)/0.6) - var(NIR %*% f2) )
 
-rep      <- 500
-MSE      <- matrix(NaN, nrow = rep, ncol = 9)
-MSE_avrg = c()
 
 
-for(j in seq(10,20,1)){
-  for(i in 1 : rep){
+bspline_function <- function(rep){
+    CV_container_spline  <- c()
+    for(order in seq(8,9,1)){
+        for(j in seq(11,12,1)){
 
-    test = sample(1:n_obs, size = test_size, replace = FALSE)
+            CV_container  <- matrix(NaN, nrow = rep, ncol = 4)
 
-    #each true beta and variance
-    Y1_1 <- NIR %*% f1 + rnorm(n_obs, 0, sigma_eps_squared1_1)
-    Y1_2 <- NIR %*% f1 + rnorm(n_obs, 0, sigma_eps_squared1_2)
-    Y2_1 <- NIR %*% f2 + rnorm(n_obs, 0, sigma_eps_squared2_1)
-    Y2_2 <- NIR %*% f2 + rnorm(n_obs, 0, sigma_eps_squared2_2)
+            for(i in 1 : rep){
+                #each true beta and variance
+                Y1_1 <- as.numeric(NIR %*% f1 + rnorm(n_obs, 0, 1) * sigma_eps_squared1_1)
+                Y1_2 <- as.numeric(NIR %*% f1 + rnorm(n_obs, 0, 1) * sigma_eps_squared1_2)
+                Y2_1 <- as.numeric(NIR %*% f2 + rnorm(n_obs, 0, 1) * sigma_eps_squared2_1)
+                Y2_2 <- as.numeric(NIR %*% f2 + rnorm(n_obs, 0, 1) * sigma_eps_squared2_2)
+        
+                data = t(NIR)
+
+                #print(dim(NIR))
+                smallbasis      <- create.bspline.basis(rangeval = c(0, length(grid)), nbasis = as.numeric(j), norder = as.numeric(order))
+                smooth_basis_fd <- smooth.basis(y = data, fdParobj=smallbasis)$fd
+                xfdlist = list(smooth_basis=smooth_basis_fd)
+                betabasis1 <- create.constant.basis(c(0, 60))
+                betafd1    <- fd(0, betabasis1)
+                betafdPar1 <- fdPar(betafd1)
+                #betafd2    <- create.bspline.basis(rangeval = c(0, length(grid)), nbasis = as.numeric(j), norder = as.numeric(order) )
+                betafdPar2  <- fdPar(smallbasis)
+                betalist <- list(smooth_basis_fd_data=betafdPar2)
     
-    Y1_1 <- as.numeric(Y1_1)
-    Y1_2 <- as.numeric(Y1_2)
-    Y2_1 <- as.numeric(Y2_1)
-    Y2_2 <- as.numeric(Y2_2)
+                f_regress1_1 <- fRegress.CV(y = Y1_1, xfdlist, betalist)
+                f_regress1_2 <- fRegress.CV(y = Y1_2, xfdlist, betalist)
+                f_regress2_1 <- fRegress.CV(y = Y2_1, xfdlist, betalist)
+                f_regress2_2 <- fRegress.CV(y = Y2_2, xfdlist, betalist)
+        
+                CV_container[i,1] <- f_regress1_1$SSE.CV
+                CV_container[i,2] <- f_regress1_2$SSE.CV
+                CV_container[i,3] <- f_regress2_1$SSE.CV
+                CV_container[i,4] <- f_regress2_2$SSE.CV
 
-    Y1_1train <- Y1_1[-test]
-    Y1_2train <- Y1_2[-test]
-    Y2_1train <- Y2_1[-test]
-    Y2_2train <- Y2_2[-test]
-
-    Y1_1test <- Y1_1[test]
-    Y1_2test <- Y1_2[test]
-    Y2_1test <- Y2_1[test]
-    Y2_2test <- Y2_2[test]
-    data = t(NIR)
-    X_train = data[,-test]
-    X_test = data[,test]
-
-    #print(dim(NIR))
-    smallbasis      <- create.bspline.basis(rangeval = c(0, length(grid)), nbasis = j, 8)
-    smooth_basis_fd_train <- smooth.basis(y = X_train, fdParobj=smallbasis)$fd
-    smooth_basis_fd_test <- smooth.basis(y = X_test, fdParobj=smallbasis)$fd
-
-    xfdlist = list(smooth_basis=smooth_basis_fd_train)
-    betabasis1 <- create.constant.basis(c(0, (60-length(test))))
-    betafd1    <- fd(0, betabasis1)
-    betafdPar1 <- fdPar(betafd1)
-    betafd2    <- create.bspline.basis(rangeval = c(0, length(grid)), nbasis = j, 8)
-    betafdPar2  <- fdPar(betafd2)
-    betalist <- list(smooth_basis_fd=betafdPar2)
-
-    f_regress1_1 <- fRegress(y = Y1_1train, xfdlist, betalist)
-    f_regress1_2 <- fRegress(y = Y1_2train, xfdlist, betalist)
-    f_regress2_1 <- fRegress(y = Y2_1train, xfdlist, betalist)
-    f_regress2_2 <- fRegress(y = Y2_2train, xfdlist, betalist)
-    
-    prediction1_1 <- predict.fRegress(object = f_regress1_1, newdata = list(data = smooth_basis_fd_test))
-    prediction1_2 <- predict.fRegress(object = f_regress1_2, newdata = list(data = smooth_basis_fd_test))
-    prediction2_1 <- predict.fRegress(object = f_regress2_1, newdata = list(data = smooth_basis_fd_test))
-    prediction2_2 <- predict.fRegress(object = f_regress2_2, newdata = list(data = smooth_basis_fd_test))
-    
-    MSE[i,1] <- mean((prediction1_1 - Y1_1test)^2)
-    MSE[i,2] <- mean((prediction1_2 - Y1_2test)^2)
-    MSE[i,3] <- mean((prediction2_1 - Y2_1test)^2)
-    MSE[i,4] <- mean((prediction2_2 - Y2_2test)^2)
-    
-    
-
-################################################################################
-
-    simulated_pcaObj_train = pca.fd(smooth_basis_fd_train, nharm = nharm,  centerfns = TRUE)
-    simulated_pcaObj_test = pca.fd(smooth_basis_fd_test, nharm = nharm, centerfns = TRUE)
-    data = simulated_pcaObj_train$scores
-    f_regress_pca <- lm(Y1_1train ~ data)
-    data = simulated_pcaObj_test$scores
-    prediction1_1 <- predict.lm(object = f_regress_pca, data.frame(data = data))
-
-    data = simulated_pcaObj_train$scores
-    f_regress_pca <- lm(Y1_2train ~ data)
-    data = simulated_pcaObj_test$scores
-    prediction1_2 <- predict.lm(object = f_regress_pca, data.frame(data = data))
-
-    data = simulated_pcaObj_train$scores
-    f_regress_pca <- lm(Y2_1train ~ data)
-    data = simulated_pcaObj_test$scores
-    prediction2_1 <- predict.lm(object = f_regress_pca, data.frame(data = data))
-
-    data = simulated_pcaObj_train$scores
-    f_regress_pca <- lm(Y2_2train ~ data)
-    data = simulated_pcaObj_test$scores
-    prediction2_2 <- predict.lm(object = f_regress_pca, data.frame(data = data))
-    
-    MSE[i,5] <- mean((prediction1_1 - Y1_1test)^2)
-    MSE[i,6] <- mean((prediction1_2 - Y1_2test)^2)
-    MSE[i,7] <- mean((prediction2_1 - Y2_1test)^2)
-    MSE[i,8] <- mean((prediction2_2 - Y2_2test)^2)
-    
-    
-  }#end of reps
-
-    scaled_MSE <- colMeans(MSE)
-    # scalling by var(X%*% tur_fun)
-    for (i in seq(1,7,2)){
-        scaled_MSE[i] = scaled_MSE[i] / var(NIR %*% f1)
+            }
+            scaled_MSE <- colMeans(CV_container)
+            scaled_MSE[5] = j
+            scaled_MSE[6] = order
+            CV_container_spline <- rbind(CV_container_spline, scaled_MSE)
+        }
     }
-    for (i in seq(2,8,2)){
-        scaled_MSE[i] = scaled_MSE[i] / var(NIR %*% f2)
-    }
-    scaled_MSE[9] = j
-    
-    
-  
-    MSE_avrg <- rbind(MSE_avrg, scaled_MSE)
-  
+    colnames(CV_container_spline) = c("f1_e1_spline", "f1_e2_spline", "f2_e1_spline", "f2_e2_spline", "n_basis", "n_order")
+    return(CV_container_spline)
 }
-colnames(MSE_avrg) = c("f1_e1_spline", "f1_e2_spline", "f2_e1_spline", "f2_e2_spline", "f1_e1_fpcr", "f1_e2_fpcr", "f2_e1_fpcr", "f2_e2_fpcr", "n_basis")
-MSE_avrg
+
+fpcr_function <- function(rep){
+    CV_container_spline  <- c()
+    train.control <- trainControl(method = "cv", number = 10)
+    for(order in seq(8,9,1)){
+        for(j in seq(11,12,1)){
+
+            CV_container  <- matrix(NaN, nrow = rep, ncol = 5)
+
+            for(i in 1 : rep){
+                #each true beta and variance
+                Y1_1 <- as.numeric(NIR %*% f1 + rnorm(n_obs, 0, 1) * sigma_eps_squared1_1)
+                Y1_2 <- as.numeric(NIR %*% f1 + rnorm(n_obs, 0, 1) * sigma_eps_squared1_2)
+                Y2_1 <- as.numeric(NIR %*% f2 + rnorm(n_obs, 0, 1) * sigma_eps_squared2_1)
+                Y2_2 <- as.numeric(NIR %*% f2 + rnorm(n_obs, 0, 1) * sigma_eps_squared2_2)
+        
+                data = t(NIR)
+                
+                #print(dim(NIR))
+                smallbasis      <- create.bspline.basis(rangeval = c(0, length(grid)), nbasis = as.numeric(j), norder = as.numeric(order))
+                smooth_basis_fd <- smooth.basis(y = data, fdParobj=smallbasis)$fd
+
+                simulated_pcaObj = pca.fd(smooth_basis_fd, nharm = nharm, centerfns = TRUE)
+
+         
+                dataframe1_1 = data.frame(Y1_1, simulated_pcaObj$scores)
+                CV_container[i,1] <- train(Y1_1 ~., data = dataframe1_1, method = "lm", trControl = train.control)$results[[2]]
+
+                dataframe1_2 = data.frame(Y1_2, simulated_pcaObj$scores)
+                CV_container[i,2] <- train(Y1_2 ~., data = dataframe1_2, method = "lm", trControl = train.control)$results[[2]]
+                
+                dataframe2_1 = data.frame(Y2_1, simulated_pcaObj$scores)
+                CV_container[i,3] <- train(Y2_1 ~., data = dataframe2_1, method = "lm", trControl = train.control)$results[[2]]
+
+                dataframe2_2 = data.frame(Y2_2, simulated_pcaObj$scores)
+                CV_container[i,4] <- train(Y2_2 ~., data = dataframe2_2, method = "lm", trControl = train.control)$results[[2]]
+
+                CV_container[i,5] <- sum(simulated_pcaObj$varprop)
+
+            }
+            scaled_MSE <- colMeans(CV_container)
+            scaled_MSE[6] = j
+            scaled_MSE[7] = order
+            CV_container_spline <- rbind(CV_container_spline, scaled_MSE)
+        }
+    }
+    colnames(CV_container_spline) = c("f1_e1_fpcr", "f1_e2_fpcr", "f2_e1_fpcr", "f2_e2_fpcr","varprop" ,"n_basis", "n_order")
+    return(CV_container_spline)
+}
