c13df303742aa32114b8cd35c6d7feb324ba0123
JakobJuergens <jakob.r.juergens@gmail.com>
2021-12-09 23:48:25 +0100

    B-spline plot

--- Code/Plot_Generators/BSpline_basis_plot.R
+++ Code/Plot_Generators/BSpline_basis_plot.R
@@ -2,51 +2,35 @@
 
 # Load packages
 library(tidyverse)
+library(fda)
 
 # setup for plot generation
-n_funcs <- 6
 n_points <- 1000
-
-# function factory that generates the basis functions
-basis_func <- function(i){
-  if(i == 1){
-    ret_func <- function(x){return(1)}
-  } else
-  if(i %% 2 == 0){
-      ret_func <- function(x){return(sqrt(2)*cos(pi * i * x))}
-  } else
-  if(i > 1 && i %% 2 == 1){
-    ret_func <- function(x){return(sqrt(2)*sin(pi * (i-1) * x))}
-  } else{
-    stop('Malformed Index chosen.')
-  }
-}
+knots <- seq(from = 0, to = 1, length.out = 8)
+order <- 3
 
 # Create points for calculation of basis functions
 points <- seq(from = 0, to = 1, length.out = n_points)
 
+# function factory that generates the basis functions
+bspline_basis <- create.bspline.basis(rangeval = c(0,1), norder = order,
+                                        breaks = knots)
+
 # Calculate basis functions at points
-res_list <- list()
-for(i in 1:n_funcs){
-  tmp_func <- basis_func(i)
-  res_list[[i]] <- unlist(
-    map(.x = points, .f = tmp_func)
-    )
-}
+res_matrix <- eval.basis(evalarg = points, basisobj = bspline_basis)
 
 # convert to tibble
-res_tibble <- cbind(tibble(x = points),
-                    as_tibble(res_list, .name_repair = 'unique'))
-names(res_tibble) <- c('x', paste0('S_', 1:n_funcs))
+res_tibble <- cbind(x = points, as_tibble(res_matrix))
+names(res_tibble) <- c('x', paste0('S_', 1:dim(res_matrix)[2]))
 
 # pivot_longer to allow for easy plotting
 plot_tibble <- res_tibble %>% 
   pivot_longer(cols = !x, names_to = 'Function', values_to = 'Value')
 
 # generate plot
-fourier_plot <- ggplot(data = plot_tibble) +
+bspline_plot <- ggplot(data = plot_tibble) +
   geom_line(aes(x = x, y = Value, col = Function)) +
-  ggtitle("Fourier Basis") +
+  ggtitle("B-spline Basis") +
   theme_light() +
   #scale_colour_brewer(palette="Set1") + 
   theme(legend.position="bottom",
@@ -57,5 +41,5 @@ fourier_plot <- ggplot(data = plot_tibble) +
         legend.text = element_text(size = 20))
 
 # save plot in appropriate folder
-ggsave(filename = '../Graphics/Fourier_Basis.pdf', plot = fourier_plot,
+ggsave(filename = '../Graphics/Bspline_Basis.pdf', plot = bspline_plot,
        width = 20, height = 8, units = "in", dpi = 600)
