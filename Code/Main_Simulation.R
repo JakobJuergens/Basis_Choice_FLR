c6d3ff410503c4f5e7bb78ba1d1a75228cdf7177
JakobJuergens <jakob.r.juergens@gmail.com>
2022-01-09 20:55:56 +0100

    Simulation Overwork

---
+++ Code/Main_Simulation.R
@@ -0,0 +1,65 @@
+##### Set up R session for simulation #####
+
+### load libraries
+suppressMessages(library(refund))
+suppressMessages(library(MASS))
+suppressMessages(library(tidyverse))
+suppressMessages(library(fda))
+suppressMessages(library(fpca))
+suppressMessages(library(caret))
+
+### load data set
+data(gasoline)
+octane <- (gasoline$octane)
+NIR <- as.matrix(gasoline$NIR)
+
+##### Define Simulation Parameters #####
+### set up "global" variables
+n_obs <- 60
+nharm <- 4
+n_var <- 400
+grid <- seq(0, 1, length = n_var + 1)
+
+### set up coefficient "functions" / error terms
+# smooth
+f1 <- 2 * sin(0.5 * pi * grid) + 4 * sin(1.5 * pi * grid) + 5 * sin(2.5 * pi * grid)
+# bumpy
+f2 <- 1.5 * exp(-0.5 * (grid - 0.3)^2 / 0.02^2) - 4 * exp(-0.5 * (grid - 0.45)^2 / 0.015^2) + 8 * exp(-0.5 * (grid - 0.6)^2 / 0.02^2) - exp(-0.5 * (grid - 0.8)^2 / 0.03^2)
+# two different variances of error
+sigma_eps_squared1_1 <- as.numeric((var(NIR %*% f1) / 0.9) - var(NIR %*% f1))
+sigma_eps_squared1_2 <- as.numeric((var(NIR %*% f1) / 0.6) - var(NIR %*% f1))
+sigma_eps_squared2_1 <- as.numeric((var(NIR %*% f2) / 0.9) - var(NIR %*% f2))
+sigma_eps_squared2_2 <- as.numeric((var(NIR %*% f2) / 0.6) - var(NIR %*% f2))
+
+##### import simulation functions #####
+# this depends on the grid variable above
+source("Simulation_Functions.R")
+### load code from other source files
+source("data_generator.R")
+source("k_fold_CV_function.R")
+
+##### Perform Simulations #####
+
+test_bspline_function <- bspline_function(rep = 500, my_data = NULL, n_obs = 200, seed = 100, debug = TRUE)
+saveRDS(test_bspline_function,file = "Results/bspline_sim_partial/rep500_n_obs200_seed100.csv")
+
+test_fourier_function <- fourier_function(rep = 1000, my_data = NULL, n_obs = 200, seed = 100, debug = TRUE)
+saveRDS(test_fourier_function,file = "Results/fourier_sim_partial/n_obs200_seed100.csv")
+
+test_fpcr <- fpcr_function(rep = 1000, my_data = NULL, n_obs = 200, seed = 100, debug = TRUE)
+saveRDS(test_fpcr,file="Results/pca_bspline_sim_partial/n_obs200_seed100.csv")
+
+test_fpcr2 <- fpcr_fourier_function(rep = 1000, my_data = NULL, n_obs = 200, seed = 100, debug = TRUE)
+saveRDS(test_fpcr2, file = "Results/pca_fourier_sim_partial/n_obs200_seed100.csv")
+
+test_bspline_function <- bspline_function(rep = 1000, my_data = NIR, n_obs = 60)
+# write.table(test_bspline_function,file="Results/test_bspline_expansion_NIR.csv")
+
+test_fourier_function <- fourier_function(rep = 1000, my_data = NIR, n_obs = 60)
+# write.table(test_fourier_function,file="Results/test_fourier_expansion_NIR.csv")
+
+test_fpcr <- fpcr_function(rep = 1000, my_data = NIR, n_obs = 60)
+# write.table(test_fpcr,file="Results/test_fpcr_bsplines_NIR.csv")
+
+test_fpcr2 <- fpcr_fourier_function(rep = 1000, my_data = NIR, n_obs = 60)
+# write.table(test_fpcr2, file = "Results/test_fpcr_fourier_NIR.csv")
--- Code/data_generator.R
+++ Code/data_generator.R
@@ -4,17 +4,17 @@ library(refund)
 library(MASS)
 library(tidyverse)
 
-# gasoline load data set
-data(gasoline)
-NIR <- as.matrix(gasoline$NIR)
-
-# get dimensions
-n_wl <- dim(NIR)[2]
-
 # define curve-generator function
 NIR_curve_generator <- function(n = 1, n_harmonics = 4, n_order = 4,
                                 n_knots = 50, plot = FALSE) {
 
+  # gasoline load data set
+  data(gasoline)
+  NIR <- as.matrix(gasoline$NIR)
+  
+  # get dimensions
+  n_wl <- dim(NIR)[2]
+  
   # define container for output
   curve_matrix <- matrix(data = NA, ncol = n_wl, nrow = n)
 
@@ -62,43 +62,44 @@ NIR_curve_generator <- function(n = 1, n_harmonics = 4, n_order = 4,
     tibble(observation = paste0("Observation_", 1:n)),
     as_tibble(
       (scores_realizations %*% t(pc_grid_vals)) +
-        matrix(data = rep(mean_function, times = n), nrow = n, byrow = TRUE)
+        matrix(data = rep(mean_function, times = n), nrow = n, byrow = TRUE),
+      .name_repair = 'unique'
     )
   )
 
   names(new_realizations) <- c("observation", colnames(NIR))
 
   # if plot == TRUE plot the new observations
-  if (plot) {
-    new_realizations_plot <- new_realizations %>%
-      pivot_longer(cols = !observation, names_to = "wavelength", values_to = "value") %>%
-      mutate(wavelength = as.numeric(str_split(wavelength, " ") %>% map_chr(., 1)))
-
-    print(ggplot(data = new_realizations_plot) +
-      geom_line(aes(x = wavelength, y = value, col = observation)) +
-      theme_light() +
-      theme(legend.position = "none"))
-  }
+  # if (plot) {
+  #   new_realizations_plot <- new_realizations %>%
+  #     pivot_longer(cols = !observation, names_to = "wavelength", values_to = "value") %>%
+  #     mutate(wavelength = as.numeric(str_split(wavelength, " ") %>% map_chr(., 1)))
+  # 
+  #   print(ggplot(data = new_realizations_plot) +
+  #     geom_line(aes(x = wavelength, y = value, col = observation)) +
+  #     theme_light() +
+  #     theme(legend.position = "none"))
+  # }
 
   return(new_realizations)
 }
 
 ##### plot original data #####
-plot_original <- FALSE
-if (plot_original) {
-  NIR_tibble <- cbind(
-    tibble(observation = paste0("Observation_", 1:(dim(NIR)[1]))),
-    as_tibble(NIR)
-  )
-
-  names(NIR_tibble) <- c("observation", colnames(NIR))
-
-  NIR_plot <- NIR_tibble %>%
-    pivot_longer(cols = !observation, names_to = "wavelength", values_to = "value") %>%
-    mutate(wavelength = as.numeric(str_split(wavelength, " ") %>% map_chr(., 1)))
-
-  ggplot(data = NIR_plot) +
-    geom_line(aes(x = wavelength, y = value, col = observation)) +
-    theme_light() +
-    theme(legend.position = "none")
-}
+# plot_original <- FALSE
+# if (plot_original) {
+#   NIR_tibble <- cbind(
+#     tibble(observation = paste0("Observation_", 1:(dim(NIR)[1]))),
+#     as_tibble(NIR)
+#   )
+# 
+#   names(NIR_tibble) <- c("observation", colnames(NIR))
+# 
+#   NIR_plot <- NIR_tibble %>%
+#     pivot_longer(cols = !observation, names_to = "wavelength", values_to = "value") %>%
+#     mutate(wavelength = as.numeric(str_split(wavelength, " ") %>% map_chr(., 1)))
+# 
+#   ggplot(data = NIR_plot) +
+#     geom_line(aes(x = wavelength, y = value, col = observation)) +
+#     theme_light() +
+#     theme(legend.position = "none")
+# }
