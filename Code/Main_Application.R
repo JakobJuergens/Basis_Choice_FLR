b4610bba3cb4bb50142a5435859e7cfc4dcda114
JakobJuergens <jakob.r.juergens@gmail.com>
2022-02-02 09:53:42 +0100

    bspline fpcr nharm 4 sim

--- Code/Main_Application.R
+++ Code/Main_Application.R
@@ -16,16 +16,16 @@ bspline_appl <- bspline_appl_function(fold_size = 5, rep = 250, seed = 100, debu
 saveRDS(object = bspline_appl, file = 'Results/Paper/Application/bspline_appl.RDS')
 write_csv(x = bspline_appl, file = 'Results/Paper/Application/bspline_appl.csv')
 
+fourier_appl <- fourier_appl_function(fold_size = 5, rep = 250, seed = 100, even_basis = FALSE, debug = TRUE)
+saveRDS(object = fourier_appl, file = 'Results/Paper/Application/fourier_appl.RDS')
+write_csv(x = fourier_appl, file = 'Results/Paper/Application/fourier_appl.csv')
+
+monomial_appl <- monomial_appl_function(fold_size = 5, rep = 250, seed = 100, debug = TRUE)
+saveRDS(object = monomial_appl, file = 'Results/Paper/Application/monomial_appl.RDS')
+write_csv(x = monomial_appl, file = 'Results/Paper/Application/monomial_appl.csv')
+
 ### Not yet implemented only for structure
 
-# fourier_appl <- fourier_appl_function(fold_size = 5, rep = 250, seed = 100, even_basis = FALSE, debug = TRUE)
-# saveRDS(object = fourier_appl, file = 'Results/Paper/Application/fourier_appl.RDS')
-# write_csv(x = fourier_appl, file = 'Results/Paper/Application/fourier_appl.csv')
-# 
-# monomial_appl <- monomial_appl_function(fold_size = 5, rep = 250, seed = 100, debug = TRUE)
-# saveRDS(object = monomial_appl, file = 'Results/Paper/Application/monomial_appl.RDS')
-# write_csv(x = monomial_appl, file = 'Results/Paper/Application/monomial_appl.csv')
-# 
 # ### Run Application for fpcr
 # bspline_fpcr_appl <- bspline_fpcr_appl_function(fold_size = 5, rep = 250, seed = 100, debug = TRUE)
 # saveRDS(object = bspline_fpcr_appl, file = 'Results/Paper/Application/bspline_fpcr_appl.RDS')
